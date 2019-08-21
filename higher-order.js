5d64b2d91a8ae1e0e208ce1d8411fda89777fd7e
Tate Stone <tatejamesstone@gmail.com>
2019-08-21 17:02:05 -0600

    Finished higher order

--- higher-order.js
+++ higher-order.js
@@ -146,10 +146,14 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal  = purchases.reduce(function(acc, el){
-  console.log(el.owner)
+let bobsTotal = purchases.reduce(function(acc, el){
+  console.log(acc)
   if(el.owner === 'Bob'){
-   acc += el.price}
+    console.log('Bob')
+   return (acc + el.price)
+  } else {
+    return acc
+  }
 }, 0)
 
 
